2e2b5b47b1c994155974a86169831e96e8565239
Facundo Arce <facundo.ea07@gmail.com>
2021-09-04 22:14:25 -0300

    FIRE agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { QuienSoyComponent } from './quien-soy/quien-soy.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule
   ],
   providers: [],
   bootstrap: [AppComponent]

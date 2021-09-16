bc45fe0062a4dba6e9e9200749cc4d685a9e6451
Facundo Arce <facundo.ea07@gmail.com>
2021-09-16 01:53:31 -0300

    Ahorcado completo

--- src/app/pages/home/pages/ahorcado/ahorcado.component.ts
+++ src/app/pages/home/pages/ahorcado/ahorcado.component.ts
@@ -7,9 +7,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorcadoComponent implements OnInit {
 
-  constructor() { }
+  arrayPalabras:string[] = ["PLATENTA","SOL","MARTE","NEPTURNO","TIERRA","SISTEMA","GALAXIA"];
+  palabraElegida:string[];
+  palabraArmada:string[];
+  cantidadLetrasCorectas:number = 0;
 
-  ngOnInit(): void {
+  vidas:number[] = [3,2,1];
+  tecladoStatus:boolean = false;
+  menuVolveraJugarDerrota:boolean = false;
+  menuVolveraJugarVictoria:boolean = false;
+
+  constructor() {
+    this.palabraElegida = (this.arrayPalabras[Math.floor(Math.random() * this.arrayPalabras.length)]).split('');
+    this.palabraArmada = this.palabraElegida.map<string>(m => m = "");
+  }
+
+  ngOnInit(): void {}
+
+  abecedario(){
+    return ["A","B","C","D","E","F",
+    "G","H","I","J","K","L","M",
+    "N","O","P","Q","R","S","T","U",
+    "V","W","X","Y","Z"];
+  }
+
+  verificarLetra(letra : string){
+    let elemento = document.getElementById("btn" + letra);
+    
+  }
+
+  onButtonClick(event : MouseEvent){
+    let value = (event.target as HTMLButtonElement).innerHTML;
+    var indices = this.obtenerTodasLasAparicionesDeUnaLetra(value);
+
+    if(indices.length > 0){
+      indices.forEach(index => {
+        //document.getElementById("txtLetra" + index)!.innerHTML = value; 
+        this.palabraArmada[index] = value;
+        this.cantidadLetrasCorectas++;
+      });
+    }
+    else{
+      this.vidas.pop();
+    }
+    //Derrota
+    if(this.vidas.length == 0){
+      this.tecladoStatus = true;
+      this.menuVolveraJugarDerrota = true;
+    }
+    //Victoria
+    //if(this.cantidadLetrasCorectas == this.palabraElegida.length){
+      if(JSON.stringify(this.palabraArmada) == JSON.stringify(this.palabraElegida)){
+      this.tecladoStatus = true;
+      this.menuVolveraJugarVictoria = true;
+    }
+
+    (event.target as HTMLButtonElement).disabled = true;
+  }
+
+  onVolveraJugar(){
+    this.vidas = [3,2,1];
+    this.menuVolveraJugarVictoria = false;
+    this.menuVolveraJugarDerrota = false;
+    this.palabraElegida = (this.arrayPalabras[Math.floor(Math.random() * this.arrayPalabras.length)]).split('');
+    this.palabraArmada = this.palabraElegida.map<string>(m => m = "");
+    this.tecladoStatus = false;
+  }
+
+  private obtenerTodasLasAparicionesDeUnaLetra(element:string):number[]{
+    var indices = [];
+    var idx = this.palabraElegida.indexOf(element);
+    while (idx != -1) {
+      indices.push(idx);
+      idx = this.palabraElegida.indexOf(element, idx + 1);
+    }
+    return indices;
   }
 
 }
